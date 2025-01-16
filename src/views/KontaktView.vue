cc2536162d3df10f0333861dd7905c5d1fce6bf2
Annas Hossain <107994538+AnnasHossain@users.noreply.github.com>
2025-01-16 10:51:03 +0100

    Update KontaktView.vue

--- src/views/KontaktView.vue
+++ src/views/KontaktView.vue
@@ -2,7 +2,6 @@
   <div class="kontakt">
     <img alt="Vue logo" src="../assets/logo.png">
     <h1> Dein Bangla Shop Father & Sons GmbH & Co.KG ... was auch immer der Name sein soll</h1>
-    <h2> vielleicht EHA's Family Shop als Shop Name: E -> Emdad, H-> Hossain A -> Annas & Ayas & Abida und das "kleine" s für die kleine Sadia </h2>
     <h2>Hier werden die Kontaktdaten des Unternehmen stehen usw.</h2>
   </div>
 </template>
